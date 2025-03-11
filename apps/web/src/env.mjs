f700a7800794e59dcaea1d02ed24e1c49dcc443a
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2025-03-12 06:02:27 +0700

    hotfix: env variable

--- apps/web/src/env.mjs
+++ apps/web/src/env.mjs
@@ -3,8 +3,6 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    NEXT_PUBLIC_DEVELOPMENT_URL: z.string().url().min(1),
-    NEXT_PUBLIC_PRODUCTION_URL: z.string().url().min(1),
     NEXT_PUBLIC_GITHUB_ID: z.string().min(1),
     NEXT_PUBLIC_GITHUB_SECRET: z.string().min(1),
     NEXT_PUBLIC_GOOGLE_ID: z.string().min(1),
@@ -13,10 +11,14 @@ export const env = createEnv({
     NEXTAUTH_URL: z.string().url().min(1),
   },
   client: {
+    NEXT_PUBLIC_DEVELOPMENT_URL: z.string().url().min(1),
+    NEXT_PUBLIC_PRODUCTION_URL: z.string().url().min(1),
     NEXT_PUBLIC_SUPABASE_URL: z.string().url().min(1),
     NEXT_PUBLIC_SUPABASE_KEY: z.string().min(1),
   },
   experimental__runtimeEnv: {
+    NEXT_PUBLIC_DEVELOPMENT_URL: process.env.NEXT_PUBLIC_DEVELOPMENT_URL,
+    NEXT_PUBLIC_PRODUCTION_URL: process.env.NEXT_PUBLIC_PRODUCTION_URL,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
   },
